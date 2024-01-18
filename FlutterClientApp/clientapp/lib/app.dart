41051737f909cd4b2b302034589c9a7cc2a162cd
Nathan Faria <nathanfaria607@gmail.com>
2024-01-18 15:45:43 -0300

    feat: clean flutter app and add appbar with simple text

---
+++ FlutterClientApp/clientapp/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'home/home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
