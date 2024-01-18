3ca59f2b6a7064adce5b27b775bf567c357d054c
Nathan Faria <nathanfaria607@gmail.com>
2024-01-18 15:30:06 -0300

    feat: add employee service

---
+++ src/main/java/br/nathan/api/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package br.nathan.api.services;
+
+import br.nathan.api.models.Employee;
+import br.nathan.api.port.EmployeeRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeService {
+    private final EmployeeRepo repo;
+
+    public EmployeeService(EmployeeRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Employee> findAll(){
+        return this.repo.findAll();
+    }
+
+    public Optional<Employee> findById(int id){
+        return this.repo.findById(id);
+    }
+
+}
