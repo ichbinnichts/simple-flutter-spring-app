c309e6eff9e4bbc84536daedb0c1b281a193d9b3
Nathan Faria <nathanfaria607@gmail.com>
2024-01-19 11:11:50 -0300

    feat: add delete and update pages

---
+++ FlutterClientApp/clientapp/lib/employee/deleteEmployee.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DeleteEmployee extends StatefulWidget {
+  const DeleteEmployee({super.key});
+
+  @override
+  State<DeleteEmployee> createState() => _DeleteEmployeeState();
+}
+
+class _DeleteEmployeeState extends State<DeleteEmployee> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
